8577fdb2fc97165e02f5ca071a493ff136fbee8b
Spandan2208 <spandan2208@gmail.com>
2025-08-07 16:01:19 +0530

    completed till backend

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,8 @@
-import Message from "../models/Message";
+import Message from "../models/Message.js";
+import user from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
+
 
 // Get all users except the logged-in user
 export const getUsersForSidebar = async (req, res) => {
@@ -56,6 +60,38 @@ export const markMessagesAsSeen = async (req, res) => {
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message });
-        
-    }
+        }
 }
+
+// Send message to selected user
+export const sendMessage = async (req, res) => {
+try {
+    const {text, image} = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+        const uploadResponse = await cloudinary.uploader.upload(image);
+        imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = await Message.create({
+        senderId,
+        receiverId,
+        text,
+        image: imageUrl
+    });
+
+    const receiverSocketId = userSocketMap[receiverId];
+    if (receiverSocketId) {
+        io.to(receiverSocketId).emit("getMessage", newMessage);
+    }
+    res.json({ success: true, newMessage });
+
+} catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+}
+
+}
+
--- server/server.js
+++ server/server.js
@@ -4,10 +4,37 @@ import cors from 'cors';
 import http from 'http';
 import {connectDB} from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
+import { Server } from 'socket.io';
 
 //create Express app and HTTP server
 const app = express();
 const server = http.createServer(app);
+
+//setup socket.io
+const io = new Server(server, {
+    cors: {origin: "*"}
+});
+ // Store online users
+export const userSocketMap = {};
+export { io };
+  // {userId: socketId}
+
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User connected: " , userId);
+
+    if (userId) userSocketMap[userId] = socket.id;  
+
+    // Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected: ", userId);
+        delete userSocketMap[userId];  // Remove user from online users
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));  // Emit updated online users
+    });
+});
 
 //middleware setup
 app.use(express.json({limit: '4mb'}));
@@ -16,6 +43,7 @@ app.use(cors());
 //routes setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 //Connect to mongoDB
 await connectDB();
